7c0c44914ec696951613f54f54d0ad8aaf09777b
Vashchenia Leonid <dcs.lvv.87@gmail.com>
2022-10-09 22:13:27 +0300

    tt

--- src/index.js
+++ src/index.js
@@ -1,67 +1,81 @@
 import './css/styles.css';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import { fetchCountries } from './js/fetchCountries';
-// import countriesListTemplate from './templates/countriesListTemplate.hbs';
-// import countrieTemplate from './templates/countrieTemplate.hbs';
-import debounce from 'lodash.debounce';
-// const debounce = require('lodash.debounce');
+import Notiflix from 'notiflix';
+import { fetchCountries } from './fetchCountries';
+var debounce = require('lodash.debounce');
 
 const DEBOUNCE_DELAY = 300;
-const NOTIFY_TIMEOUT = 2000; //ms
-const FLAG_OFFSET = 34;
 
-const inputRef = document.querySelector('#search-box');
-const countriesListRef = document.querySelector('.country-list');
-const countrieInfoRef = document.querySelector('.country-info');
+const refs = {
+  searchForm: document.querySelector('#search-box'),
+  listOfCountries: document.querySelector('.country-list'),
+  infoBox: document.querySelector('.country-info'),
+};
 
-inputRef.addEventListener('input', debounce(onTextInput, DEBOUNCE_DELAY));
-countriesListRef.addEventListener('click', onCountriesListClick);
+refs.searchForm.addEventListener(
+  'input',
+  debounce(onCheckInputValueTarget, DEBOUNCE_DELAY)
+);
 
-function onTextInput({ target: { value } }) {
-  value = value.trim();
-  clearCountriesHTML();
-  if (value === '') {
-    return;
+function onCheckInputValueTarget(e) {
+  const inputValue = e.target.value.trim();
+  if (inputValue === '') {
+    onClearHTMLmarkup();
   }
-  fetchCountries(value).then(drawCountries).catch(onResponseError);
-}
-
-function onCountriesListClick(event) {
-  inputRef.value = event.target.textContent.slice(FLAG_OFFSET);
-  inputRef.dispatchEvent(new Event('input'));
-}
-
-function drawCountries(countriesList) {
-  if (countriesList.length > 10) {
-    Notify.info(`Too many matches found. Please enter a more specific name`, {
-      timeout: NOTIFY_TIMEOUT,
+  fetchCountries(inputValue)
+    .then(counrtryName => onRenderMarkup(counrtryName))
+    .catch(() => {
+      onClearHTMLmarkup();
+      Notiflix.Notify.failure('Oops, there is no country with that name');
     });
-  } else if (countriesList.length > 1) {
-    countriesListRef.insertAdjacentHTML(
-      'beforeend',
-      countriesListTemplate(countriesList)
-    );
-  } else if (countriesList.length === 1) {
-    countriesListRef.insertAdjacentHTML(
-      'beforeend',
-      countrieTemplate(countriesList[0])
+}
+
+function onClearHTMLmarkup() {
+  refs.listOfCountries.innerHTML = '';
+  refs.infoBox.innerHTML = '';
+}
+
+function onRenderMarkup(counrtryName) {
+  onCreateMaxCountries(counrtryName);
+  onCreateMarkupCountry(counrtryName);
+  onCreateMarkupListOfCountries(counrtryName);
+}
+
+function onCreateMaxCountries(counrtryName) {
+  console.log(counrtryName);
+  if (counrtryName.length > 10) {
+    onClearHTMLmarkup();
+    return Notiflix.Notify.info(
+      'Too many matches found. Please enter a more specific name.'
     );
   }
 }
-
-function onResponseError(error) {
-  if (error.message === '404') {
-    Notify.failure(`Oops, there is no country with that name`, {
-      timeout: NOTIFY_TIMEOUT,
-    });
-    return;
+function onCreateMarkupCountry(counrtryName) {
+  if (counrtryName.length === 1) {
+    onClearHTMLmarkup();
+    refs.listOfCountries.innerHTML = counrtryName
+      .map(country => {
+        return `<li class='country-list__item--bigger'><p class='country-list__text--bigger'><img class='country-list__image--bigger' src=${country.flags.svg}><b>${country.name.official}</b></p>`;
+      })
+      .join('');
+    refs.infoBox.innerHTML = counrtryName
+      .map(country => {
+        return `<p><b>Capital:</b> ${
+          country.capital
+        }</p><p><b>Population:</b> ${
+          country.population
+        }</p><p><b>Languages:</b> ${Object.values(country.languages)}</p>`;
+      })
+      .join('');
   }
-  Notify.failure(`Response error: ${error}`, {
-    timeout: NOTIFY_TIMEOUT,
-  });
 }
 
-function clearCountriesHTML() {
-  countriesListRef.innerHTML = '';
-  countrieInfoRef.innerHTML = '';
+function onCreateMarkupListOfCountries(counrtryName) {
+  if (counrtryName.length >= 2 && counrtryName.length <= 10) {
+    onClearHTMLmarkup();
+    refs.listOfCountries.innerHTML = counrtryName
+      .map(country => {
+        return `<li class='country-list__item'><p class='country-list__text'><img class='country-list__image' src=${country.flags.svg}><b>${country.name.official}</b></p>`;
+      })
+      .join('');
+  }
 }
